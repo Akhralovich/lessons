9fbbd05d6457f8e311b45b558856d3852a5c983f
Akhralovich <ahralovich85@mail.ru>
2023-01-15 12:35:09 +0300

    add homework

--- hkjhhhk.py
+++
@@ -1,7 +0,0 @@
-a = [1, 3, 3, 4, 6, 5, 6]
-b = []
-for i in a:
-    if i < 5:
-        b.append(i)
-print(b)
-
--- lesson 04-1 (homework).py
+++
@@ -1,8 +0,0 @@
-#Дан список my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], выведите все элементы, которые меньше 5.
-
-my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-new_list = []
-for element in my_list:
-    if element  < 5:
-        new_list.append(element)
-print(new_list)
