3a9187ac7b23b5fd975043168bfdfcc43722a82b
Akhralovich <ahralovich85@mail.ru>
2023-01-15 20:49:54 +0300

    add

---
+++ lesson_08/library/car.py
@@ -0,0 +1,33 @@
+'''
+Создать класс Car.
+Атрибуты: марка, модель, год выпуска, скорость (по умолчанию 0).
+Методы: увеличить скорости (скорость +5), уменьшение скорости (скорость -5), стоп (сброс скорости на 0),
+отображение скорости, задний ход (изменение знака скорости).
+'''
+
+class Car:
+    brand = None
+    model = None
+    year = None
+    speed = None
+
+    def __init__(self, brand, model, year, speed):
+        self.brand, self.model, self.year, self.speed = brand, model, year, speed
+
+    def display_info(self):
+        print(f"Марка: {self.brand} модель: {self.model} год выпуска: {self.year}")
+
+    def speed_increase(self):
+        self.speed += 5
+    def speed_decrease(self):
+        self.speed -= 5
+    def stop(self):
+        self.speed = 0
+    def display_speed(self):
+        print(f'{self.speed} км/ч')
+    def reverse(self):
+        self.speed = -5
+    def car_to_100(self):
+        while self.speed != 100:
+            self.speed_increase()
+            self.display_speed()
