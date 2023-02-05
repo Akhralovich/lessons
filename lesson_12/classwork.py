c072ba817099119a55784d9744a71f2b8aa51a63
Akhralovich <ahralovich85@mail.ru>
2023-02-05 21:05:35 +0300

    add

--- lesson_12/classwork.py
+++ lesson_12/classwork.py
@@ -1,16 +1,17 @@
-from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import create_database, database_exists
 
-from lesson_12.models import Base, User
+from lesson_12.models import Base, User, Address, Profile
 
-DB_PATH = Path(__file__).resolve().parent / "my_database.sqlite3"
-DB_ECHO = True
-
+DB_USER = "lexa"
+DB_PASSWORD = "lexa"
+DB_NAME = "lexa"
 
 if __name__ == "__main__":
-    engine = create_engine(f"sqlite:////{DB_PATH}", echo=DB_ECHO)
+    engine = create_engine(
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}"
+    )
     if not database_exists(engine.url):
         create_database(engine.url)
 
@@ -20,4 +21,20 @@ if __name__ == "__main__":
 
     user = User(email="test@test.com", password="password")
     session.add(user)
+
     session.commit()
+
+    address = Address(user_id=user.id, city="Minsk", address="Test")
+    session.add(address)
+
+    profile = Profile(user_id=user.id, phone="+375292999999", age=20)
+    session.add(profile)
+
+    user = session.query(User).filter(User.email == "test@test.com").first()
+    user.password = "new_password"
+    session.add(user)
+    session.commit()
+
+    result = session.query(Profile).filter(Profile.age >= 15)
+    for x in result:
+        print(x)
